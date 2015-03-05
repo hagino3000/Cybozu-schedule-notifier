e3b9dbd5ccd0470c5e6c4de89c8b2cdc2e4111ec
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2015-03-05 14:34:43 +0900

    Get rid of growl

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var Cybozu = require('./thirdparty/cybozu-connect'),
-    growl = require('growl'),
+    NotificationCenter = require('node-notifier/notifiers/notificationcenter'),
     SETTINGS = require('./settings');
 
 Cybozu.init(SETTINGS.JQUERY_PATH, function(API) {
 
   if (API) {
     console.log(API);
-    growl('Cheking Cybozu schedules.......');
+    console.log(SETTINGS);
 
     var con = new API.CybozuConnect.App(SETTINGS.URL,
                                         SETTINGS.LOGIN_NAME,
@@ -28,25 +28,46 @@ Cybozu.init(SETTINGS.JQUERY_PATH, function(API) {
     }, true);
 
     if (events.users && events.users[con.user.id]) {
-      console.info(events.users);
-      events.users[con.user.id].forEach(function(s) {
-        if (!s.allDay) {
-          glowlEvent(s);
+      var notifyEvents = [];
+      (function processEvents(){
+        var ev = events.users[con.user.id].pop();
+        if (ev) {
+          if (!ev.allDay) {
+            notifyEvents.push({
+              title: ev.title,
+              time: (new Date(ev.start)).toLocaleTimeString() 
+                    + " - " 
+                    + (new Date(ev.end)).toLocaleTimeString(),
+              body: ev.description,
+              location: ev.facilities[0] ? ev.facilities[0].name : '場所の指定無し',
+              url: SETTINGS.URL + "/schedule/view?event=" + ev.id
+            });
+          }
+          processEvents();
+        } else {
+          notify(notifyEvents);
         }
-      });
+      })();
     }
   } else {
     console.info('failed to init cybozu api');
   }
 });
 
-
-function glowlEvent(e) {
-  console.info(e.title);
-  var start = new Date(e.start);
-  growl(e.title, {
-    title: 'Cybozu: ' + start.toLocaleTimeString(),
-    sticky: true
-  });
+function notify(data) {
+    data.forEach(function(entry) {
+        var notifier = new NotificationCenter();
+        notifier.notify({
+            title: entry.title,
+            subtitle: entry.time,
+            message: entry.location,
+            icon: __dirname + '/image/icon.png',
+            open: entry.url
+        });
+    });
 }
+
+process.on('uncaughtexception', function(e) {
+  console.error(e);
+});
 
---
+++ image/README.md
@@ -0,0 +1,2 @@
+See
+http://www.softicons.com/system-icons/crystal-project-icons-by-everaldo-coelho/mimetypes-schedule-icon
