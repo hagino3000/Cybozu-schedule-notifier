059663e207c4b13fe040c39108c829f8ad387fe4
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2015-03-05 16:02:27 +0900

    Cosmetic

--- settings.sample.js
+++ settings.sample.js
@@ -1,5 +1,5 @@
 // Cybozu Entry URL
-exports.URL           = 'https://YOUR_HOSTED_GAROON/grn.exe';
+exports.URL = 'https://YOUR_HOSTED_GAROON/grn.exe';
 
 // jQuery
 exports.JQUERY_PATH = 'http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js';
