ce7779f05843fb76bc1649f8646306124d9c555e
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2015-03-06 11:50:32 +0900

    Cosmetic

--- settings.sample.js
+++ settings.sample.js
@@ -5,5 +5,5 @@ exports.URL = 'https://YOUR_HOSTED_GAROON/grn.exe';
 exports.JQUERY_PATH = 'http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js';
 
 // Keychain entry of Garoon credential
-exports.LOGIN_ACCOUNT = ''
-exports.KEYCHAIN_ENTRY_NAME = 'garoon.hoge.fuga'
+exports.LOGIN_ACCOUNT = '';
+exports.KEYCHAIN_ENTRY_NAME = 'garoon.hoge.fuga';
